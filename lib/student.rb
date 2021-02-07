19d3ba97ffd335658e5db251f3301d080950dcb4
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-07 03:31:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 9
     SQL
-    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   end
 
   def self.all_students_below_12th_grade
